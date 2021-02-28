cd9089bbb5257c5933f48b60ce743712ef096d0b
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2021-02-28 19:32:13 -0300

    Update
    Starting the Design Responsive and menu mobile created.

---
+++ js/menuMobile.js
@@ -0,0 +1,15 @@
+const btnMenu = document.querySelector('.menuMobile__btnBar');
+const btnCloseMenu = document.querySelector('.menu__btnClose');
+
+const menuNav = document.querySelector('.menuMobile__menu');
+const bgBlack = document.querySelector('.bgBlack');
+
+btnMenu.addEventListener('click', () => {
+    menuNav.classList.add('menuMobile__menu--active');
+    bgBlack.classList.add('bgBlack--active');
+});
+
+btnCloseMenu.addEventListener('click', () => {
+    menuNav.classList.remove('menuMobile__menu--active');
+    bgBlack.classList.remove('bgBlack--active');
+});
