283f8d32d80d8340ffc2161a9788600cfd79e8bf
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2021-03-04 12:41:46 -0300

    Update
    Insert a acessibility function in the menu mobile

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -1,20 +1,58 @@
 const btnMenu = document.querySelector('.menuMobile__btnBar');
 const btnCloseMenu = document.querySelector('.menu__btnClose');
+const linksMobile = document.querySelectorAll('[data-linksMobile]');
 
 const menuNav = document.querySelector('.menuMobile__menu');
+const homeLink = document.querySelector('.menu__items--home');
 const bgBlack = document.querySelector('.bgBlack');
 
+
+menuNav.classList.add('disable');
+
+
 btnMenu.addEventListener('click', () => {
-    menuNav.classList.add('menuMobile__menu--active');
-    bgBlack.classList.add('bgBlack--active');
+    menuNav.classList.remove('disable');
+
+    setTimeout(() => {
+        menuNav.classList.add('menuMobile__menu--active');
+        bgBlack.classList.add('bgBlack--active');
+        btnCloseMenu.focus();
+
+        linksMobile[5].addEventListener('focusout', focusOnlyMenu);
+    }, 100);
 });
 
-btnCloseMenu.addEventListener('click', () => {
+function closeMenu() {
     menuNav.classList.remove('menuMobile__menu--active');
     bgBlack.classList.remove('bgBlack--active');
+    
+    removeEventsListeners();
+    btnMenu.focus();
+        
+    setTimeout(() => {
+        menuNav.classList.add('disable');
+    }, 500);
+} 
+
+btnCloseMenu.addEventListener('click', () => {
+    closeMenu();  
+});
+
+menuNav.addEventListener('keyup', (event) => {
+    if(event.keyCode === 27){
+        closeMenu();
+    }
 });
 
 bgBlack.addEventListener('click', () => {
-    menuNav.classList.remove('menuMobile__menu--active');
-    bgBlack.classList.remove('bgBlack--active');
+    closeMenu();
 });
+
+
+function focusOnlyMenu() {
+    linksMobile[0].focus();
+}
+
+function removeEventsListeners() {
+    linksMobile[5].removeEventListener('focusout', focusOnlyMenu, false);
+}
